19f86a48a88b3b25cbb8467143662e28bb311f99
Gobind Singh <android.gobind@gmail.com>
2022-02-24 19:54:06 +0530

    about page structure ready

---
+++ src/assets/texts/about.js
@@ -0,0 +1,11 @@
+export const left_heading = `ON POINT**CREATIVE**IS SPOT ON**BUSINESS`;
+export const right_heading = `BUILDING DATA DRIVEN, PEOPLE-CENTERED SOLUTIONS FOR BRANDS THAT MATTER, SINCE 1989`;
+const col_1 = [
+  "Digital",
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+];
+const col_2 = [
+  "Print",
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+];
+export const cols = [col_1, col_2];
--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,10 +1,69 @@
 import "./About.css";
 import React from "react";
+import { left_heading, right_heading, cols } from "../../assets/texts/about";
+import si from "../../assets/images/si.webp";
+import si2 from "../../assets/images/googlelogo.png";
+import si3 from "../../assets/images/reactlogo.png";
+
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 export default function about() {
   return (
     <div id="about" className="full-page">
-      about
+      <div className="left-side">
+        <h2>
+          {left_heading.split("**").map((t, i) => {
+            return (
+              <span key={i}>
+                {t}
+                <br />
+              </span>
+            );
+          })}
+        </h2>
+        <img src={si} alt="" />
+        <div className="companies">
+          <Carousel
+            responsive={{
+              default: {
+                breakpoint: { max: 9999, min: 0 },
+                items: 3,
+              },
+            }}
+            swipeable={false}
+            draggable={false}
+            showDots={false}
+            infinite={true}
+            autoPlay={true}
+            autoPlaySpeed={2000}
+            keyBoardControl={true}
+            customTransition="all 0.5s ease-in-out"
+            transitionDuration={500}
+            arrows={false}
+          >
+            <img src={si} alt="" className="comapny-img" />
+            <img src={si2} alt="" className="comapny-img" />
+            <img src={si3} alt="" className="comapny-img" />
+          </Carousel>
+        </div>
+      </div>
+      <div className="right-side">
+        <h3>{right_heading}</h3>
+        <div className="right-cols">
+          {cols.map((data, ind) => {
+            return (
+              <div className={"col-" + (ind + 1)} key={ind}>
+                <h3>{data[0]}</h3>
+                <p>{data[1]}</p>
+              </div>
+            );
+          })}
+        </div>
+        <a href="/#about">
+          <button>ALL SERVICES</button>
+        </a>
+      </div>
     </div>
   );
 }

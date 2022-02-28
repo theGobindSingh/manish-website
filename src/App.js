2f4518a4d12a48abd8c1be84f363543a2ec4b9f1
Gobind Singh <android.gobind@gmail.com>
2022-02-28 17:01:04 +0530

    scrolling features done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import Nav from "./components/Nav/Nav";
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
 import Contact from "./pages/contact/Contact";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const target = document.querySelector("*");
+    target.addEventListener("wheel", (event) => {
+      const toLeft = event.deltaY < 0 && target.scrollLeft > 0;
+      const toRight =
+        event.deltaY > 0 &&
+        target.scrollLeft < target.scrollWidth - target.clientWidth;
+      if (toLeft || toRight) {
+        event.preventDefault();
+        target.scrollLeft += event.deltaY;
+      }
+    });
+  });
   return (
     <div id="App" className="App">
       <Nav />
-      <div className="blank-element" style={{ width: "13vw" }} />
+      <div className="blank-element" style={{ width: "10vw" }} />
       <Home />
       <About />
       <Contact />
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,12 +2,51 @@ import "./Nav.css";
 import React from "react";
 
 export default function Nav() {
+  function forScroll(elemID) {
+    try {
+      // document.querySelector("#" + elemID).scrollIntoView({
+      //   behavior: "smooth",
+      //   block: "start",
+      // });
+      const blue = document.querySelector("#" + elemID);
+      let position = blue.getBoundingClientRect();
+      window.scrollTo({
+        top: position.top,
+        left: position.left + window.scrollX - 60,
+        behavior: "smooth",
+      });
+    } catch {}
+  }
   return (
     <nav id="mainNav">
-      <a href="/#home">Home</a>
-      <a href="/#about">About</a>
-      <a href="/#work">Work</a>
-      <a href="/#contact">Contact us</a>
+      <a
+        onClick={() => {
+          forScroll("home");
+        }}
+      >
+        Home
+      </a>
+      <a
+        onClick={() => {
+          forScroll("about");
+        }}
+      >
+        About
+      </a>
+      <a
+        onClick={() => {
+          forScroll("work");
+        }}
+      >
+        Work
+      </a>
+      <a
+        onClick={() => {
+          forScroll("contact");
+        }}
+      >
+        Contact us
+      </a>
     </nav>
   );
 }
